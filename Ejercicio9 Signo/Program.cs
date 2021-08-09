5b19e7b93b59a3b192c64d7c2531eba639466961
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-08 20:41:40 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicio9 Signo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ejercicio9_Signo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este es el numero:"+Signo(0.1));
+        }
+        public static int Signo(double num)
+        {
+            int numeroEntero;
+            if (num == 0)
+            {
+                return numeroEntero =0;
+            }else if (num <= 0.0)
+            {
+                numeroEntero = -1;
+                return numeroEntero;
+            }
+            else
+            {
+                numeroEntero = 1;
+                return numeroEntero;
+            }
+           
+        }
+    }
+}
